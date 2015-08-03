a06335f88d0bd22df90d7ec055378b1fdc63e40c
cesar <cesar@romero.ws>
2015-08-02 20:33:43 -0700

    adding items to a list - client side only.

--- static/js/rank.js
+++ static/js/rank.js
@@ -1,1 +1,17 @@
-angular.module('ranker', []);
+angular.module('ranker', [])
+    .controller(
+        'listCreationCtrl',
+        function($log, $scope){
+            // start with no candidates
+            $scope.candidates = [];
+
+            $scope.add = function(event) {
+                if(event.which === 13) {
+                    $log.debug(event);
+                    $scope.candidates.push({name:$scope.newCandidate});
+                    $scope.newCandidate = '';
+
+                    $log.info('candidates after push: ' + angular.toJson($scope.candidates));
+                }
+            };
+        });
